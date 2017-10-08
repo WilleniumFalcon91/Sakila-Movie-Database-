3003c9e7c00331035858acd28509e87f6586f20d
William Elliott <wcelliott1991@gmail.com>
2017-10-08 12:53:50 -0400

    shows difference between JOIN and WHERE functions

--- skeleton.sql
+++ skeleton.sql
@@ -60,4 +60,26 @@ GROUP BY
     1
 ;
 
+SELECT
+    ac.customer_id, 
+    ac.fav_color,
+    rc.num_purchases
+FROM
+    active_customer ac, 
+    reward_customer rc
+WHERE
+    ac.customer_id = rc.customer_id
+;
+
+SELECT 
+    ac.customer_id, 
+    ac.fav_color, 
+    rc.num_purchases
+FROM 
+    active_customer ac 
+    JOIN reward_customer rc ON ac.customer_id = rc.customer_id
+;
+
+
+
 
